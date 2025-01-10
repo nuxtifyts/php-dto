5c8e7b592d1eaac1c43eb6a7974860eb9b70d024
Fa-BRAIK <faroukbraik17@gmail.com>
2025-01-10 18:59:41 -0500

    Add utility methods to retrieve values from arrays with type checks and defaults

--- src/Support/Arr.php
+++ src/Support/Arr.php
@@ -6,7 +6,6 @@ final readonly class Arr
 {
     /**
      * @param array<array-key, mixed> $array
-     * @param string $key
      * @param array<array-key, mixed> $default
      *
      * @return array<array-key, mixed>
@@ -16,6 +15,78 @@ final readonly class Arr
         $value = $array[$key] ?? null;
 
         return is_array($value) ? $value : $default;
+    }
+
+    /** 
+     *  @param array<array-key, mixed> $array
+     */
+    public static function getStringOrNull(array $array, string $key): ?string
+    {
+        $value = $array[$key] ?? null;
+
+        return is_string($value) ? $value : null;
+    }
+
+    /** 
+     * @param array<array-key, mixed> $array
+     */
+    public static function getString(array $array, string $key, string $default = ''): string
+    {
+        return self::getStringOrNull($array, $key) ?? $default;
+    }
+
+        /** 
+     * @param array<array-key, mixed> $array
+     */
+    public static function getIntegerOrNull(array $array, string $key): ?int
+    {
+        $value = $array[$key] ?? null;
+
+        return is_int($value) ? $value : null;
+    }
+
+    /** 
+     * @param array<array-key, mixed> $array
+     */
+    public static function getInteger(array $array, string $key, int $default = 0): int
+    {
+        return self::getIntegerOrNull($array, $key) ?? $default;
+    }
+
+    /** 
+     * @param array<array-key, mixed> $array
+     */
+    public static function getFloatOrNull(array $array, string $key): ?float
+    {
+        $value = $array[$key] ?? null;
+
+        return is_float($value) ? $value : null;
+    }
+
+    /** 
+     * @param array<array-key, mixed> $array
+     */
+    public static function getFloat(array $array, string $key, float $default = 0.0): float
+    {
+        return self::getFloatOrNull($array, $key) ?? $default;
+    }
+
+    /** 
+     * @param array<array-key, mixed> $array
+     */
+    public static function getBooleanOrNull(array $array, string $key): ?bool
+    {
+        $value = $array[$key] ?? null;
+
+        return is_bool($value) ? $value : null;
+    }
+
+    /** 
+     * @param array<array-key, mixed> $array
+     */
+    public static function getBoolean(array $array, string $key, bool $default = false): bool
+    {
+        return self::getBooleanOrNull($array, $key) ?? $default;
     }
 
     /**
--- tests/Unit/Support/ArrTest.php
+++ tests/Unit/Support/ArrTest.php
@@ -62,6 +62,198 @@ final class ArrTest extends UnitCase
                 ],
                 [],
             ],
+            'get string existing key, invalid value' => [
+                'getString',
+                [
+                    'array' => ['key' => 1],
+                    'key' => 'key',
+                ],
+                '',
+            ],
+            'get string existing key, valid value' => [
+                'getString',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'key',
+                ],
+                'value',
+            ],
+            'get string non-existing key' => [
+                'getString',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'nonExistingKey',
+                ],
+                '',
+            ],
+            'get string or null existing key, invalid value' => [
+                'getStringOrNull',
+                [
+                    'array' => ['key' => 1],
+                    'key' => 'key',
+                ],
+                null,
+            ],
+            'get string or null existing key, valid value' => [
+                'getStringOrNull',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'key',
+                ],
+                'value',
+            ],
+            'get string or null non-existing key' => [
+                'getStringOrNull',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'nonExistingKey',
+                ],
+                null,
+            ],
+            'get integer existing key, invalid value' => [
+                'getInteger',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'key',
+                ],
+                0,
+            ],
+            'get integer existing key, valid value' => [
+                'getInteger',
+                [
+                    'array' => ['key' => 1],
+                    'key' => 'key',
+                ],
+                1,
+            ],
+            'get integer non-existing key' => [
+                'getInteger',
+                [
+                    'array' => ['key' => 1],
+                    'key' => 'nonExistingKey',
+                ],
+                0,
+            ],
+            'get integer or null existing key, invalid value' => [
+                'getIntegerOrNull',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'key',
+                ],
+                null,
+            ],
+            'get integer or null existing key, valid value' => [
+                'getIntegerOrNull',
+                [
+                    'array' => ['key' => 1],
+                    'key' => 'key',
+                ],
+                1,
+            ],
+            'get integer or null non-existing key' => [
+                'getIntegerOrNull',
+                [
+                    'array' => ['key' => 1],
+                    'key' => 'nonExistingKey',
+                ],
+                null,
+            ],
+            'get float existing key, invalid value' => [
+                'getFloat',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'key',
+                ],
+                0.0,
+            ],
+            'get float existing key, valid value' => [
+                'getFloat',
+                [
+                    'array' => ['key' => 1.1],
+                    'key' => 'key',
+                ],
+                1.1,
+            ],
+            'get float non-existing key' => [
+                'getFloat',
+                [
+                    'array' => ['key' => 1.1],
+                    'key' => 'nonExistingKey',
+                ],
+                0.0,
+            ],
+            'get float or null existing key, invalid value' => [
+                'getFloatOrNull',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'key',
+                ],
+                null,
+            ],
+            'get float or null existing key, valid value' => [
+                'getFloatOrNull',
+                [
+                    'array' => ['key' => 1.1],
+                    'key' => 'key',
+                ],
+                1.1,
+            ],
+            'get float or null non-existing key' => [
+                'getFloatOrNull',
+                [
+                    'array' => ['key' => 1.1],
+                    'key' => 'nonExistingKey',
+                ],
+                null,
+            ],
+            'get boolean existing key, invalid value' => [
+                'getBoolean',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'key',
+                ],
+                false,
+            ],
+            'get boolean existing key, valid value' => [
+                'getBoolean',
+                [
+                    'array' => ['key' => true],
+                    'key' => 'key',
+                ],
+                true,
+            ],
+            'get boolean non-existing key' => [
+                'getBoolean',
+                [
+                    'array' => ['key' => true],
+                    'key' => 'nonExistingKey',
+                ],
+                false,
+            ],
+            'get boolean or null existing key, invalid value' => [
+                'getBooleanOrNull',
+                [
+                    'array' => ['key' => 'value'],
+                    'key' => 'key',
+                ],
+                null,
+            ],
+            'get boolean or null existing key, valid value' => [
+                'getBooleanOrNull',
+                [
+                    'array' => ['key' => true],
+                    'key' => 'key',
+                ],
+                true,
+            ],
+            'get boolean or null non-existing key' => [
+                'getBooleanOrNull',
+                [
+                    'array' => ['key' => true],
+                    'key' => 'nonExistingKey',
+                ],
+                null,
+            ],
         ];
     }
 
